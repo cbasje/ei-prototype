6b2700f9a3b9c8e4539416417bfb374f9874e786
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-01-15 17:59:04 +0100

    Improve PWA support

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
     <Html data-theme="pastel" lang="en" />
     <Title>EI prototype</Title>
-    <Link rel="icon" href="favicon.png" />
+    <Link rel="icon" href="favicon.ico" />
+    <Meta name="theme-color" content="#FFFFFF" />
+    <Link rel="manifest" href="/app.webmanifest" />
+    <link rel="apple-touch-icon" href="/images/icon-ios.png" />
 
     <NuxtPage />
 </template>
