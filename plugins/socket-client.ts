39006dbd8d6ecd65b54a34c86997c623a3447807
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-01-15 18:00:08 +0100

    Add uuid

--- plugins/socket-client.ts
+++ plugins/socket-client.ts
@@ -1,11 +1,14 @@
 import io from "socket.io-client";
+import { v4 as uuid } from "uuid";
 
 export default defineNuxtPlugin(() => {
     const config = useRuntimeConfig();
+    const id = uuid();
 
     const socket = io(config.public.socketURL, {
         transports: ["websocket", "polling"],
         reconnectionAttempts: 3,
+        query: { id },
     });
 
     return {
